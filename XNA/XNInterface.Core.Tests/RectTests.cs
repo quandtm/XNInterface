011eb8efdf722980850883e9df598aac20df11ed
Michael Quandt <michael@mquandt.com>
2013-10-03 04:11:45 +1000

    Added more Rect tests

--- XNA/XNInterface.Core.Tests/RectTests.cs
+++ XNA/XNInterface.Core.Tests/RectTests.cs
@@ -28,5 +28,41 @@ namespace XNInterface.Core.Tests
             Assert.AreEqual(r.Position, new Vec2(1, 2));
             Assert.AreEqual(r.Size, new Vec2(3, 4));
         }
+
+        [TestMethod]
+        public void TestFloatParent()
+        {
+            var r = new Rect(new Vec2(1, 0.5f, Vec2.Vec2ValueType.Percentage),
+                new Vec2(0.5f, 1, Vec2.Vec2ValueType.Percentage));
+            r.Calculate(100, 100);
+            Assert.AreEqual(100, r.Position.RealX);
+            Assert.AreEqual(50, r.Position.RealY);
+            Assert.AreEqual(50, r.Size.RealX);
+            Assert.AreEqual(100, r.Size.RealY);
+        }
+
+        [TestMethod]
+        public void TestVec2Parent()
+        {
+            var r = new Rect(new Vec2(1, 0.5f, Vec2.Vec2ValueType.Percentage),
+                new Vec2(0.5f, 1, Vec2.Vec2ValueType.Percentage));
+            r.Calculate(new Vec2(100, 100));
+            Assert.AreEqual(100, r.Position.RealX);
+            Assert.AreEqual(50, r.Position.RealY);
+            Assert.AreEqual(50, r.Size.RealX);
+            Assert.AreEqual(100, r.Size.RealY);
+        }
+
+        [TestMethod]
+        public void TestRectParent()
+        {
+            var r = new Rect(new Vec2(1, 0.5f, Vec2.Vec2ValueType.Percentage),
+                new Vec2(0.5f, 1, Vec2.Vec2ValueType.Percentage));
+            r.Calculate(new Rect(0, 0, 100, 100));
+            Assert.AreEqual(100, r.Position.RealX);
+            Assert.AreEqual(50, r.Position.RealY);
+            Assert.AreEqual(50, r.Size.RealX);
+            Assert.AreEqual(100, r.Size.RealY);
+        }
     }
 }
