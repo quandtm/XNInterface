42a4a7c4248338f0e016382505ba8e4289ada208
Michael Quandt <michael@mquandt.com>
2013-10-03 04:06:12 +1000

    Added Rect ctors and tests

---
+++ XNA/XNInterface.Core.Tests/RectTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XNInterface.Core.Tests
+{
+    [TestClass]
+    public class RectTests
+    {
+        [TestMethod]
+        public void TestEmptyCtor()
+        {
+            var r = new Rect();
+            Assert.AreEqual(r.Position, new Vec2());
+            Assert.AreEqual(r.Size, new Vec2());
+        }
+
+        [TestMethod]
+        public void TestAllFloatCtor()
+        {
+            var r = new Rect(1, 2, 3, 4);
+            Assert.AreEqual(r.Position, new Vec2(1, 2));
+            Assert.AreEqual(r.Size, new Vec2(3, 4));
+        }
+
+        [TestMethod]
+        public void TestVec2Ctor()
+        {
+            var r = new Rect(new Vec2(1, 2), new Vec2(3, 4));
+            Assert.AreEqual(r.Position, new Vec2(1, 2));
+            Assert.AreEqual(r.Size, new Vec2(3, 4));
+        }
+    }
+}
--- src/Core/Rect.cs
+++ src/Core/Rect.cs
@@ -5,6 +5,22 @@ namespace XNInterface.Core
         public Vec2 Position { get; set; } // Will always be relative to parent
         public Vec2 Size { get; set; }
 
+        public Rect()
+            : this(0, 0, 0, 0)
+        {
+        }
+
+        public Rect(float x, float y, float width, float height)
+            : this(new Vec2(x, y), new Vec2(width, height))
+        {
+        }
+
+        public Rect(Vec2 position, Vec2 size)
+        {
+            Position = position;
+            Size = size;
+        }
+
         public void Calculate(float parentWidth, float parentHeight)
         {
             Position.Calculate(parentWidth, parentHeight);
